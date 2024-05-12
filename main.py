28ad33a665f2c562046871248b56dbd383e714a9
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2024-05-12 00:48:03 -0500

    Fix

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ def get_frames():
     while True:
         status, frame = camera.read()
         status, buffer = cv2.imencode('.jpg', frame)
-        yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n'
+        yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n'
 
 
 @app.route('/stream')
 def stream():
     return flask.Response(get_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/embed')
+def embed():
+    return flask.render_template('embed.html')
 
 
 @app.route('/')
